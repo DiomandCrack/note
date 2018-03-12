542a9786d2737e964fe0cc922586d4273324a10f
DiamondCrack <zk05161219@gmail.com>
2018-03-12 16:25:21 +0800

    update webpack babel

--- learn-webpack/webpack.config.js
+++ learn-webpack/webpack.config.js
@@ -2,6 +2,19 @@ module.exports = {
     entry:{app:'./app.js'},
     output:{
         filename:'[name].[hash:5].js'
+    },
+    module:{
+        //rulse是个数组
+        rules:[
+            {
+                //用正则匹配文件的后缀
+                test:/\.js&/,
+                //编译es6
+                use:'babel-loader',
+                //排除在外
+                exclude:'/node_modules/'
+            }
+            //babel-perset 编译的规则
+        ]
     }
-    
 }
