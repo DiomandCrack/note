f2d2637fb039be0af026baea31a834e2c3a18076
DiamondCrack <zk05161219@gmail.com>
2018-05-05 22:43:33 +0800

    indexedDB 获取数据

--- H5/H5存储/IndexedDB/IndexedDB.md
+++ H5/H5存储/IndexedDB/IndexedDB.md
@@ -28,3 +28,29 @@ indexedDB.createObjectStore(osName,{autoIncrement:true})
 
 `osName`表的名称
 `{autoIncrement:true}`主键类型为指针
+
+## 表的增删改查
+
+### 事务 IDBTransaction
+
+调用`indexedDB.transaction`方法会返回一个`IDBTransaction`对象，它含有一个`objectStore`方法
+可以让用户通过指定模式获取 `objectStore`
+
+### 创建数据
+
+```js
+//改写表的读取方式 默认是只读 osName 可以是个数组 批量修改读取模式 返回值为IDBTransaction对象
+var transaction = db.transaction(osName,'readwrite')
+//找到表
+store=transaction.objectStore(osName)
+//添加数据
+store.add({a:1})
+```
+~~所有方法的返回值都是IDBOpenRequest对象~~
+
+### 获取数据
+
+```js
+const request = store.get(id)
+const {result} = request
+```
--- H5/H5存储/IndexedDB/更新数据库/update.js
+++ H5/H5存储/IndexedDB/更新数据库/update.js
@@ -1,6 +1,6 @@
 var db = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB
 
-var requeset,result,version=1,
+var requeset,result,version=3,
   dbName='testDB'
   osName='os1'
 
@@ -9,6 +9,7 @@ var requeset,result,version=1,
     
     request.onsuccess = function () {
       db = request.result
+      console.log('db open success')
     }
     
     request.onerror = function(e){
@@ -18,6 +19,7 @@ var requeset,result,version=1,
     request.onupgradeneeded = function () {
       db = request.result
       //检查数据库中是否含有os1
+      console.log('onupgradeneeded')
       if(!db.objectStoreNames.contains(osName)){
         //没有就创建表
         //主键类型为指针
@@ -27,3 +29,34 @@ var requeset,result,version=1,
   }
 
   createDB()
+
+  var data = [{
+    name:'史莱姆',
+    id:'001',
+    hp:3
+  },{
+    name:'小蝙蝠',
+    id:'002',
+    hp:5
+  },{
+    name:'小恶魔',
+    id:'003',
+    hp:9
+  }]
+
+  function addData() {
+    var transaction = db.transaction(osName,'readwrite')
+    var store=transaction.objectStore(osName)
+    data.map(function (o) {
+      store.add(o)
+    })
+  }
+
+  function getData(id){
+    var transaction = db.transaction(osName,'readwrite')
+    var store = transaction.objectStore(osName)
+    var request = store.get(id)
+    request.onsuccess = function (){
+      console.log(request.result)
+    }
+  }
