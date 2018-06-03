1718782a0a79f6d57784c2b48d70694245da8535
DiomandCrack <zk05161219@gmail.com>
2018-06-03 21:03:30 +0800

    匹配 括号

---
+++ 数据结构/线性结构/栈/Valid-Parentheses/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Solution solution = new Solution();
+
+        System.out.println(solution.isValid("()[]{}"));
+    }
+}
