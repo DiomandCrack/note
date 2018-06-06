ac3538a0203b33247b932efc78adc16f098eff3d
DiomandCrack <zk05161219@gmail.com>
2018-06-07 07:42:49 +0800

    java 数据类型

---
+++ java/dataClass/src/CharDemo.java
@@ -0,0 +1,13 @@
+public class CharDemo {
+    public static void main(String[] args){
+        char a = 'a';
+        char ch = 65;
+        System.out.println("a="+a);
+        System.out.println("ch="+ch);
+
+        //定义变量存放unicode编码表示的字符
+
+        char c='\u005d';
+        System.out.println("c="+c);
+    }
+}
