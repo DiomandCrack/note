17bdddb4ebdbd17dcbd150f578c6bf157782085c
DiamondCrack <zk05161219@gmail.com>
2018-06-08 14:35:08 +0800

    解析代码模块路径

---
+++ learnWebpack/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports = {
+  entry:'./src/index.js',
+
+  output:{
+    path:path.resolve(__dirname,'dist'),
+    filename:'bundle.[hash].js'
+  },
+
+  module:{
+    rules:[
+      {
+        test:/\.jsx?/,
+        include:[
+          path.resolve(__dirname,'src')
+        ],
+        use:'babel-loader'
+      },
+      {
+        test:/\.css$/,
+        use:[
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      },
+    ]
+  },
+
+  resolve:{
+    modules:[
+      "node_modules",
+      path.resolve(__dirname,'src')
+    ],
+    extensions:[".wasm",".mjs",".js",".json","jsx"]
+  },
+
+  plugins:[
+    new UglifyPlugin(),
+    new HtmlWebpackPlugin({
+      filename:'index.html',
+      template:'assets/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename:"index.[hash].css",
+      chunkFilename:"[id].css"
+    })
+  ],
+}
