568d1749c8c3604e4350f7f7bb10b5ed6407851d
DiomandCrack <zk05161219@gmail.com>
2018-06-17 17:32:37 +0800

    java  数组

---
+++ java/exercise/Array/src/Tarray.java
@@ -0,0 +1,17 @@
+public class Tarray {
+    public static void main(String[] args){
+        //二维数组的声明
+        //三种形式
+        int[][] intArray;
+        float floatArray[][];
+        double[] doubleArray[];
+
+        floatArray = new float[3][];
+
+        floatArray[0] = new float[3];
+
+        floatArray[1] = new float[4];
+
+        floatArray[2] = new float[5];
+    }
+}
