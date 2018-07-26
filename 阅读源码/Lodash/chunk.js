7264200f5d37d94c92d2ddf246ff4ffd332af0f2
DiamondCrack <zk05161219@gmail.com>
2018-07-26 14:31:40 +0800

    lodash/chunk

---
+++ 阅读源码/Lodash/chunk.js
@@ -0,0 +1,41 @@
+import slice from './slice.js'
+
+/**有了slice 可以看chunk了
+ * 创建一个数组,里面的元素被拆成 'size' 大小的数组，如果不能均匀分割,最后一个数组就是 *剩下元素的集合
+ * Creates an array of elements split into groups the length of `size`.
+ * If `array` can't be split evenly, the final chunk will be the remaining
+ * elements.
+ *
+ * @since 3.0.0
+ * @category Array
+ * @param {Array} array The array to process.
+ * @param {number} [size=1] The length of each chunk
+ * @returns {Array} Returns the new array of chunks.
+ * @example
+ *
+ * chunk(['a', 'b', 'c', 'd'], 2)
+ * // => [['a', 'b'], ['c', 'd']]
+ *
+ * chunk(['a', 'b', 'c', 'd'], 3)
+ * // => [['a', 'b', 'c'], ['d']]
+ */
+function chunk(array, size) {
+  size = Math.max(size, 0)
+  const length = array == null ? 0 : array.length
+  if (!length || size < 1) {
+    return []
+  }
+  let index = 0
+  let resIndex = 0
+  /**
+   * 向上取整 Math.ceil()
+   */
+  const result = new Array(Math.ceil(length / size))
+
+  while (index < length) {
+    result[resIndex++] = slice(array, index, (index += size))
+  }
+  return result
+}
+
+export default chunk
